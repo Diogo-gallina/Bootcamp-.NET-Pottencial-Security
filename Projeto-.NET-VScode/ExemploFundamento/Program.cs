e0c695e22faf2d0fdcdb82c29394188e6cd4bb02
Diogo-gallina <diogogallina20@gmail.com>
2022-09-12 19:25:42 -0300

    Anotações da aula Sintaxe e Tipos de dados em C#

---
+++ Projeto-.NET-VScode/ExemploFundamento/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExemploFundamento.Models;
+
+DateTime dataAtual = DateTime.Now.add;
+Console.WriteLine(dataAtual);
+
+
+
+
+
+
+// string apresentacao ="Olá seja bem vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine(quantidade);
+// Console.WriteLine(altura.ToString("0.00"));
+// Console.WriteLine(preco);
+// Console.WriteLine(condicao);
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Diogo";
+// pessoa1.Idade = 18;
+// pessoa1.Apresentar();
